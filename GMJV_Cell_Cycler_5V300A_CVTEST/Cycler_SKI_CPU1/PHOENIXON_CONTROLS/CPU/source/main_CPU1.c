857214515e963a8a0910f584e09ec91092ff2850
pup99 <kms@phoenixon.com>
2021-07-16 11:13:55 +0900

    Debug 폴더 무시

--- GMJV_Cell_Cycler_5V300A_CVTEST/Cycler_SKI_CPU1/PHOENIXON_CONTROLS/CPU/source/main_CPU1.c
+++ GMJV_Cell_Cycler_5V300A_CVTEST/Cycler_SKI_CPU1/PHOENIXON_CONTROLS/CPU/source/main_CPU1.c
@@ -70,6 +70,7 @@ S_OPERATION_FILE s_Operation;
 uint16_t git_ignore_test;
 uint16_t git_ignore_test2;
 uint16_t git_ignore_test3;
+uint16_t git_ignore_test4;
 
 //#pragma CODE_SECTION(printf, "ramfuncs");
 
