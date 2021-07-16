2fd75065466d751f35fde1b2063f6eaf437a669c
pup99 <kms@phoenixon.com>
2021-07-16 10:58:25 +0900

    ignore .xml 추가

--- GMJV_Cell_Cycler_5V300A_CVTEST/Cycler_SKI_CPU1/PHOENIXON_CONTROLS/CPU/source/main_CPU1.c
+++ GMJV_Cell_Cycler_5V300A_CVTEST/Cycler_SKI_CPU1/PHOENIXON_CONTROLS/CPU/source/main_CPU1.c
@@ -68,6 +68,7 @@ extern volatile S_COMMUNICATION_TX_ALARM s_SENSING_ALARM;
 S_OPERATION_FILE s_Operation;
 
 uint16_t git_ignore_test;
+uint16_t git_ignore_test2;
 
 //#pragma CODE_SECTION(printf, "ramfuncs");
 
